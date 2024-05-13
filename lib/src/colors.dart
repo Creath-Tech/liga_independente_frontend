95cc68982d438e18279468b74713ad8e7cdc9be0
raposo8 <victinhorolim@gmail.com>
2024-05-13 18:10:44 -0300

    add: paleta de cores do app

---
+++ lib/src/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+var primarycolor = const Color(0xFF14213d);
+var secondarycolor = const Color(0xFFf4ee35);
+var errorboxcolor = const Color.fromARGB(255, 11, 22, 46);
