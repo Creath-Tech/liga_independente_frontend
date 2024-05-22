0bd55df6f04c1402bdbd52d96813abef20f9e131
Av3z <willyanf17@hotmail.com>
2024-05-21 22:26:50 -0300

    added errorColor

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 var primarycolor = const Color(0xFF14213d);
 var secondarycolor = const Color(0xFFf4ee35);
+var errorColor = const Color(0xFFba3232);
 var errorboxcolor = const Color.fromARGB(255, 11, 22, 46);
