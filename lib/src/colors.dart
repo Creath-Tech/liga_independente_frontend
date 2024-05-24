dda7c37b1583c6f330b27d637aa446041b84d26f
Av3z <willyanf17@hotmail.com>
2024-05-24 16:49:41 -0300

    added new colors and change errorboxcolor for black

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 var primarycolor = const Color(0xFF14213d);
 var secondarycolor = const Color(0xFFf4ee35);
 var errorColor = const Color(0xFFba3232);
-var errorboxcolor = const Color.fromARGB(255, 11, 22, 46);
+var sucessColor = const Color(0xFF36b651);
+var disableColor = const Color(0xFF9c9c9c);
+var errorboxcolor = const Color.fromARGB(51, 0, 0, 0);
