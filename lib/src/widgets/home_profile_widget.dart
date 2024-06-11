b35595e8d55b50131280c3677f4e854999ab9512
raposo8 <victinhorolim@gmail.com>
2024-06-11 17:41:18 -0300

    feat: added home profile widget

---
+++ lib/src/widgets/home_profile_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:liga_independente_frontend/src/colors.dart';
+
+class HomeProfile extends StatelessWidget {
+  const HomeProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: errorboxcolor,
+      height: MediaQuery.of(context).size.height / 6,
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 25, left: 20),
+                child: const CircleAvatar(
+                  radius: 30,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            child: const Center(
+              child: Text(
+                "Pessoas",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 15,
+            right: 30,
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: const Icon(
+                    Icons.filter_alt_rounded,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+                const SizedBox(width: 8), // Add some spacing manually if needed
+                GestureDetector(
+                  onTap: () {},
+                  child: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
