56f94dd75caaadf76429618e0dab9c18eec59ad5
Av3z <willyanf17@hotmail.com>
2024-06-14 12:12:19 -0300

    modify model for set data users.

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,13 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
   String? userId;
+  String? imageUrl;
   String? name;
   String? email;
   String? bio;
   List<String>? sports;
   Map<String, dynamic>? contacts;
 
+  Timestamp createdAt = Timestamp.now();
+  Timestamp updatedAt = Timestamp.now();
+
   UserModel(
       {this.userId,
+      this.imageUrl,
       this.name,
       this.email,
       this.bio = 'Amo jogar volei e futebol.',
@@ -17,6 +24,32 @@ class UserModel {
         'facebook' : 'https://facebook.com/user1',
         'instagram' : 'https://instagram.com/user1'
       }
-      });
+  });
 
+
+  UserModel.fromJson(json) {
+    userId = json['userId'];
+    imageUrl = json['imageUrl'];
+    name = json['name'];
+    email = json['email'];
+    bio = json['password'];
+    sports = json['phone'];
+    contacts = json['adress'];
+    createdAt = json['createdAt'].toDate();
+    updatedAt = json['updatedAt'].toDate();
+  }
+
+  toJson() {
+    return {
+      'userId': userId,
+      'imageUrl': imageUrl,
+      'name': name,
+      'email': email,
+      'bio': bio,
+      'sports': sports,
+      'contacts': contacts,
+      'createdAt' : createdAt,
+      'upadatedAt' : updatedAt
+    };
+  }
 }
