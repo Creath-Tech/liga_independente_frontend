11a0f1c84be7369750e77edf44fcde56199c9d2c
Av3z <willyanf17@hotmail.com>
2024-06-21 16:36:53 -0300

    get all info of database and show in profile page

--- lib/src/widgets/profile_page/profile_box.dart
+++ lib/src/widgets/profile_page/profile_box.dart
@@ -5,9 +5,10 @@ class ProfileBox extends StatelessWidget {
   final Widget child;
   final String title;
   final bool editMode;
+  final IconData icon;
   final Function() onPressed;
 
-  const ProfileBox({super.key, required this.color, required this.child, required this.title, required this.onPressed, required this.editMode});
+  const ProfileBox({super.key, required this.color, required this.child, required this.title, required this.onPressed, required this.editMode, this.icon = Icons.edit});
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,21 @@ class ProfileBox extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(title, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w700,),),
-              editMode ? Container() : IconButton(onPressed: onPressed, icon: const Icon(Icons.edit, color: Colors.white, size: 20,)),
+              editMode 
+              ? Container() 
+              : 
+
+              icon == Icons.add 
+
+              ? Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: CircleAvatar(radius: 10, backgroundColor: Colors.white, child: 
+                IconButton(padding: EdgeInsets.zero, 
+                  onPressed: onPressed, 
+                  icon: Icon(icon, color: Colors.black, size: 20,)),),
+              ) 
+
+              : IconButton(onPressed: onPressed, icon: Icon(icon, color: Colors.white, size: 20,)),
             ],
           ),
           const SizedBox(height: 15,),
