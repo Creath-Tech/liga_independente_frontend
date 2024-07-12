4dcc6dbfc915ab518d8bd13309fcd1b04dc0c8aa
Av3z <willyanf17@hotmail.com>
2024-07-11 23:32:44 -0300

    update select sports page for use remote config

--- lib/src/pages/select_sports.dart
+++ lib/src/pages/select_sports.dart
@@ -5,13 +5,33 @@ import 'package:liga_independente_frontend/src/widgets/auth_message.dart';
 import 'package:liga_independente_frontend/src/widgets/cloud_button.dart';
 import 'package:liga_independente_frontend/src/widgets/primary_button.dart';
 
-class SelectSports extends StatelessWidget {
+class SelectSports extends StatefulWidget {
   const SelectSports({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    SelectSportsController selectSportsController = SelectSportsController();
+  _SelectSportsState createState() => _SelectSportsState();
+}
 
+class _SelectSportsState extends State<SelectSports> {
+  late SelectSportsController selectSportsController;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    selectSportsController = SelectSportsController();
+    _initializeController();
+  }
+
+  Future<void> _initializeController() async {
+    await selectSportsController.loadSports();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primarycolor,
       body: SafeArea(
@@ -53,55 +73,59 @@ class SelectSports extends StatelessWidget {
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.9,
                 height: MediaQuery.of(context).size.height * 0.55,
-                child: ValueListenableBuilder<List<String>>(
-                  valueListenable: selectSportsController.selectedSports,
-                  builder: (context, selectedSports, child) {
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 1,
-                        mainAxisSpacing: 5,
-                        childAspectRatio: 3,
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : ValueListenableBuilder<List<String>>(
+                        valueListenable: selectSportsController.sports,
+                        builder: (context, sports, child) {
+                          return GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 1,
+                              mainAxisSpacing: 5,
+                              childAspectRatio: 3,
+                            ),
+                            itemCount: sports.length,
+                            itemBuilder: (context, index) {
+                              String sport = sports[index];
+                              return ValueListenableBuilder<List<String>>(
+                                valueListenable: selectSportsController.selectedSports,
+                                builder: (context, selectedSports, child) {
+                                  bool isSelected = selectedSports.contains(sport);
+                                  return GestureDetector(
+                                    onTap: () => selectSportsController.toggleSportSelection(sport),
+                                    child: CloudButton(
+                                      color: isSelected ? secondarycolor : boxColorHeader,
+                                      child: Text(sport,
+                                          style: TextStyle(
+                                              color: isSelected ? Colors.black : Colors.white)),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          );
+                        },
                       ),
-                      itemCount: selectSportsController.sports.length,
-                      itemBuilder: (context, index) {
-                        String sport = selectSportsController.sports[index];
-                        bool isSelected = selectedSports.contains(sport);
-                        return GestureDetector(
-                          onTap: () => selectSportsController
-                              .toggleSportSelection(sport),
-                          child: CloudButton(
-                            color: isSelected ? secondarycolor : boxColorHeader,
-                            child: Text(sport,
-                                style: TextStyle(
-                                    color: isSelected
-                                        ? Colors.black
-                                        : Colors.white)),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
               ),
               const SizedBox(
                 height: 50,
               ),
               Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35),
-                  child: ValueListenableBuilder(
-                    valueListenable: selectSportsController.showError,
-                    builder: (context, showError, child) {
-                      return PrimaryButton(
-                          onPressed: showError
-                              ? () {}
-                              : () =>
-                                  selectSportsController.saveSports(context),
-                          text: "CONTINUAR",
-                          color: showError ? disableColor : secondarycolor);
-                    },
-                  )),
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                child: ValueListenableBuilder(
+                  valueListenable: selectSportsController.showError,
+                  builder: (context, showError, child) {
+                    return PrimaryButton(
+                        onPressed: showError
+                            ? () {}
+                            : () => selectSportsController.saveSports(context),
+                        text: "CONTINUAR",
+                        color: showError ? disableColor : secondarycolor);
+                  },
+                ),
+              ),
             ],
           ),
         ),
